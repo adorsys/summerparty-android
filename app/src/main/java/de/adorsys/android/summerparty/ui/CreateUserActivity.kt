e7e6956ad57265824adbc69c795be09013f37aec
Drilon Reçica <drilonreqica@gmail.com>
2017-06-21 14:03:03 +0200

    Created empty Splash and CreateUser activities

---
+++ app/src/main/java/de/adorsys/android/summerparty/ui/CreateUserActivity.kt
@@ -0,0 +1,13 @@
+package de.adorsys.android.summerparty.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import de.adorsys.android.summerparty.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
