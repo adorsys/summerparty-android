57be4d7047b441d5b6d6c89ae5af35c23b6ca900
Nino Handler <nih@adorsys.de>
2017-06-21 15:28:18 +0200

    CSI_712 Added retrofit service definition

---
+++ app/src/main/java/de/adorsys/android/summerparty/network/CocktailService.kt
@@ -0,0 +1,24 @@
+package de.adorsys.android.summerparty.network
+
+import de.adorsys.android.summerparty.data.Cocktail
+import de.adorsys.android.summerparty.data.Order
+import de.adorsys.android.summerparty.data.mutable.MutableOrder
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface CocktailService {
+    @POST("/order")
+    fun createOrder(@Body mutableOrder: MutableOrder): Call<Order>
+
+    @GET("/order/{id}")
+    fun getOrder(@Path("id") id: String): Call<Order>
+
+    @get:GET("/beverage")
+    val cocktails: Call<Cocktail>
+
+    @GET("/beverage/{id}")
+    fun getCocktail(@Path("id") id: String): Call<Cocktail>
+}
