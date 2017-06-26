88a7a4b6dd58d1106ca2079caedb5444f4fb3a27
Nino Handler <nih@adorsys.de>
2017-06-26 17:07:18 +0200

    CSI_712 Updated customer class

--- app/src/main/java/de/adorsys/android/summerparty/data/Customer.kt
+++ app/src/main/java/de/adorsys/android/summerparty/data/Customer.kt
@@ -1,4 +1,4 @@
 package de.adorsys.android.summerparty.data
 
 // TODO check if fields are correct when specified
-class Customer(val id: String, val name: String)
+class Customer(val id: String, val name: String, val pushId: String)
