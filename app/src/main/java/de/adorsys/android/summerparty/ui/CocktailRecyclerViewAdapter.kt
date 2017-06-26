3ab33b3460c259cef701bf2cba11ea8a0e03580d
Nino Handler <nih@adorsys.de>
2017-06-26 17:10:48 +0200

    CSI_712 Updated adapter view holder
    * Now setting an icon for the availability
    * Implemented rough design
    * Added bind method for view holder

--- app/src/main/java/de/adorsys/android/summerparty/ui/CocktailRecyclerViewAdapter.kt
+++ app/src/main/java/de/adorsys/android/summerparty/ui/CocktailRecyclerViewAdapter.kt
@@ -4,46 +4,49 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import de.adorsys.android.summerparty.R
 import de.adorsys.android.summerparty.data.Cocktail
 
 class CocktailRecyclerViewAdapter(
-		private val cocktails: ArrayList<Cocktail>,
-		private val listener: OrderFragment.OnListFragmentInteractionListener?) : RecyclerView.Adapter<CocktailRecyclerViewAdapter.ViewHolder>() {
+        private val cocktails: ArrayList<Cocktail>,
+        private val listener: OrderFragment.OnListFragmentInteractionListener?) : RecyclerView.Adapter<CocktailRecyclerViewAdapter.ViewHolder>() {
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocktailRecyclerViewAdapter.ViewHolder {
-		val view = LayoutInflater.from(parent.context)
-				.inflate(R.layout.fragment_order, parent, false)
-		return ViewHolder(view)
-	}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocktailRecyclerViewAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_order, parent, false)
+        return ViewHolder(view)
+    }
 
-	override fun onBindViewHolder(holder: CocktailRecyclerViewAdapter.ViewHolder, position: Int) {
-		holder.item = cocktails[position]
-		holder.idView.text = cocktails[position].id
-		holder.contentView.text = cocktails[position].type?.name
+    override fun onBindViewHolder(holder: CocktailRecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bindItem(cocktails[position])
+    }
 
-		holder.view.setOnClickListener {
-			listener?.onListFragmentInteraction(holder.item as Cocktail)
-		}
-	}
+    override fun getItemCount(): Int {
+        return cocktails.size
+    }
 
-	override fun getItemCount(): Int {
-		return cocktails.size
-	}
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.id_textView) as TextView
+        val contentView: TextView = view.findViewById(R.id.name_textView) as TextView
+        val availabilityView: ImageView = view.findViewById(R.id.available_imageView) as ImageView
+        var item: Cocktail? = null
 
-	inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-		val idView: TextView
-		val contentView: TextView
-		var item: Cocktail? = null
+        fun bindItem(cocktail: Cocktail) {
+            item = cocktail
+            idView.text = cocktail.id
+            contentView.text = cocktail.name
+            availabilityView.setImageDrawable(
+                    if (cocktail.available) {
+                        view.context.getDrawable(R.drawable.ic_wb_sunny_green_24dp)
+                    } else {
+                        view.context.getDrawable(R.drawable.ic_wb_cloudy_red_24dp)
+                    })
 
-		init {
-			idView = view.findViewById(R.id.id) as TextView
-			contentView = view.findViewById(R.id.content) as TextView
-		}
-
-		override fun toString(): String {
-			return super.toString() + " '" + contentView.text + "'"
-		}
-	}
+            view.setOnClickListener {
+                listener?.onListFragmentInteraction(cocktail)
+            }
+        }
+    }
 }
