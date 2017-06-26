bdef66b9bd8200fcb58e65be19e275ec2496435c
Nino Handler <nih@adorsys.de>
2017-06-26 17:12:39 +0200

    CSI_712 Made Customer and Order parcelable to ensure they can be serialized and deserialied

--- app/src/main/java/de/adorsys/android/summerparty/data/Customer.kt
+++ app/src/main/java/de/adorsys/android/summerparty/data/Customer.kt
@@ -1,4 +1,28 @@
 package de.adorsys.android.summerparty.data
 
+import android.os.Parcel
+import android.os.Parcelable
+
 // TODO check if fields are correct when specified
-class Customer(val id: String, val name: String, val pushId: String)
+class Customer(val id: String, val name: String, val pushId: String) : Parcelable {
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Customer> = object : Parcelable.Creator<Customer> {
+            override fun createFromParcel(source: Parcel): Customer = Customer(source)
+            override fun newArray(size: Int): Array<Customer?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+    source.readString(),
+    source.readString(),
+    source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(id)
+        dest.writeString(name)
+        dest.writeString(pushId)
+    }
+}
