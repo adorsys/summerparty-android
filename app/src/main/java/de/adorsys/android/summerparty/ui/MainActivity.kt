3d39d7aaed1ed8b3708581f239c7c44a68ea47ad
Nino Handler <nih@adorsys.de>
2017-06-28 16:07:48 +0200

    CSI-710 put callbacks into MainActivity

--- app/src/main/java/de/adorsys/android/summerparty/ui/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/summerparty/ui/MainActivity.kt
@@ -11,10 +11,13 @@ import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import de.adorsys.android.summerparty.R
 import de.adorsys.android.summerparty.data.Cocktail
 import de.adorsys.android.summerparty.data.CocktailManager
-import de.adorsys.android.summerparty.data.callback.CartActionCallback
+import de.adorsys.android.summerparty.data.MutableCustomer
+import de.adorsys.android.summerparty.data.mutable.MutableOrder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,12 +27,14 @@ class MainActivity : AppCompatActivity(), OrderFragment.OnListFragmentInteractio
     private var viewPager: ViewPager? = null
     private val cocktails = ArrayList<Cocktail>()
     private val cocktailsCallback = CocktailsCallback()
+    private val orderCallback = OrderCallback()
+    private val cartActionCallback = CartActionCallback()
+    private var currentOrder: MutableOrder? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // init views
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         viewPager = findViewById(R.id.container) as ViewPager
@@ -64,7 +69,7 @@ class MainActivity : AppCompatActivity(), OrderFragment.OnListFragmentInteractio
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    private inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         private val PAGER_COUNT: Int = 2
         private val COCKTAIL_COLUMN_COUNT: Int = 1
 
@@ -87,7 +92,9 @@ class MainActivity : AppCompatActivity(), OrderFragment.OnListFragmentInteractio
         }
     }
 
-    inner class CocktailsCallback : Callback<List<Cocktail>> {
+
+    // Callbacks
+    private inner class CocktailsCallback : Callback<List<Cocktail>> {
         override fun onResponse(call: Call<List<Cocktail>>, response: Response<List<Cocktail>>) {
             cocktails.clear()
             val cocktailResponse: List<Cocktail>? = response.body()
@@ -96,10 +103,29 @@ class MainActivity : AppCompatActivity(), OrderFragment.OnListFragmentInteractio
             Log.i("TAG", "success")
         }
 
-
         override fun onFailure(call: Call<List<Cocktail>>, t: Throwable) {
             Log.i("TAG", t.message)
         }
     }
 
+    private inner class OrderCallback : Callback<Void> {
+        override fun onResponse(call: Call<Void>, response: Response<Void>) {
+            if (response.isSuccessful) {
+                Toast.makeText(this@MainActivity, "Successfully created order", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        override fun onFailure(call: Call<Void>, t: Throwable) {
+            Log.i("TAG", t.message)
+        }
+    }
+
+    private inner class CartActionCallback : View.OnClickListener {
+        override fun onClick(v: View) {
+            // TODO add item to cart instead of directly creating order
+            if (currentOrder != null) {
+                CocktailManager.INSTANCE.createOrder(currentOrder!!, orderCallback)
+            }
+        }
+    }
 }
