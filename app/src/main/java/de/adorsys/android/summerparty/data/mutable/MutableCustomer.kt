f0073faa68a9180fc97f6d84735224f16819b642
Nino Handler <nih@adorsys.de>
2017-06-30 11:21:38 +0200

    Corrected customer type from "Android" to "android"

--- app/src/main/java/de/adorsys/android/summerparty/data/mutable/MutableCustomer.kt
+++ app/src/main/java/de/adorsys/android/summerparty/data/mutable/MutableCustomer.kt
@@ -1,3 +1,3 @@
 package de.adorsys.android.summerparty.data.mutable
 
-data class MutableCustomer(val name: String, val pushId: String = "", val type: String = "Android")
+data class MutableCustomer(val name: String, val pushId: String = "", val type: String = "android")
