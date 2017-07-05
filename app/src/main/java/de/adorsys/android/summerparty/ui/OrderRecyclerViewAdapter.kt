5d48d76137ed8707aa08af46c32d1de47a95fd4f
Nino Handler <nih@adorsys.de>
2017-07-05 17:31:34 +0200

    CSI-724 created sections on order page

--- app/src/main/java/de/adorsys/android/summerparty/ui/OrderRecyclerViewAdapter.kt
+++ app/src/main/java/de/adorsys/android/summerparty/ui/OrderRecyclerViewAdapter.kt
@@ -4,32 +4,49 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import de.adorsys.android.summerparty.R
 import de.adorsys.android.summerparty.data.Order
 import de.adorsys.android.summerparty.ui.views.OrderView
 
 class OrderRecyclerViewAdapter(
-        private val orders: ArrayList<Order>) : RecyclerView.Adapter<OrderRecyclerViewAdapter.ViewHolder>() {
+        private val orders: List<Order>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderRecyclerViewAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_order, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_order, parent, false)
+        return OrderViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: OrderRecyclerViewAdapter.ViewHolder, position: Int) {
-        holder.bindItem(orders[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is OrderViewHolder -> holder.bindItem(orders[position])
+        }
     }
 
     override fun getItemCount(): Int {
         return orders.size
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val orderView: OrderView = view.findViewById(R.id.order_view) as OrderView
+    inner class OrderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val orderView = view.findViewById(R.id.order_view) as OrderView
+        private val headerView = view.findViewById(R.id.cocktail_state_header) as TextView
 
         fun bindItem(order: Order) {
             orderView.order = order
+
+            var headerText: String? = ""
+            when (order.state) {
+                "mixed" -> headerText = headerView.context.getString(R.string.order_state_mixed)
+                "delivered" -> headerText = headerView.context.getString(R.string.order_state_delivered)
+                "ordered" -> headerText = headerView.context.getString(R.string.order_state_ordered)
+            }
+
+            if (adapterPosition == 0 || order.state != orders[adapterPosition - 1].state) {
+                headerView.visibility = View.VISIBLE
+                headerView.text = headerText
+            } else {
+                headerView.visibility = View.GONE
+            }
         }
     }
 }
