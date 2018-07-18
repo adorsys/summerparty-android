2070d300c03ae1b21b4a14d02be7c5d59637aec5
Nino Handler <nih@adorsys.de>
2018-07-18 14:06:00 +0200

    Moved Post into shared module

--- things/src/main/java/de/adorsys/android/summerpartysocial/FeedFragment.kt
+++ things/src/main/java/de/adorsys/android/summerpartysocial/FeedFragment.kt
@@ -14,6 +14,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.firestore.*
+import de.adorsys.android.shared.Post
 import kotlinx.android.synthetic.main.fragment_feed.*
 import java.io.ByteArrayOutputStream
 
