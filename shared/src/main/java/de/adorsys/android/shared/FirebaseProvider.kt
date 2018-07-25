7c693fd90afab0115fe4c2e5650fd51af549ba6c
Nino Handler <nih@adorsys.de>
2018-07-25 12:43:40 +0200

    Added Firebase Storage for the images

--- shared/src/main/java/de/adorsys/android/shared/FirebaseProvider.kt
+++ shared/src/main/java/de/adorsys/android/shared/FirebaseProvider.kt
@@ -1,9 +1,15 @@
 package de.adorsys.android.shared
 
+import android.net.Uri
+import android.util.Log
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.Query
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+
+
 
 
 object FirebaseProvider {
@@ -29,5 +35,56 @@ object FirebaseProvider {
         return getFireStore()
                 .collection("summerparty")
                 .orderBy("timestamp", Query.Direction.DESCENDING)
+    }
+
+    fun uploadImage(
+            file: File,
+            progressAction: (progress: Int) -> Unit,
+            successAction: (storageReference: String) -> Unit,
+            failureAction: (e: Exception) -> Unit) {
+
+        if (file.path.isNullOrBlank()) {
+            Log.e("TAG_IMAGE_UPLOAD", "imagePath is empty or null")
+            return
+        }
+
+        val storage = FirebaseStorage.getInstance()
+        val storageReference = storage.reference
+        val imagesReference = storageReference.child("images/${file.name}")
+
+        val uploadTask = imagesReference.putFile(Uri.fromFile(file))
+        uploadTask
+                .addOnProgressListener {
+                    val rawProgress = 100.0 * it.bytesTransferred / it.totalByteCount
+                    val progress = Math.round(rawProgress).toInt()
+                    progressAction(progress)
+                }
+                .addOnSuccessListener {
+                    successAction(it.storage.path)
+                }
+                .addOnFailureListener(failureAction)
+    }
+
+    fun downloadImage(
+            fileReference: String?,
+            successAction: (file: File) -> Unit,
+            failureAction: (e: Exception) -> Unit) {
+
+        if (fileReference.isNullOrBlank()) {
+            Log.e("TAG_IMAGE_DOWNLOAD", "imagePath is empty or null")
+            return
+        }
+
+        val storage = FirebaseStorage.getInstance()
+        val storageReference = storage.reference
+        val imagesReference = storageReference.child(fileReference!!)
+
+        val localFile = File.createTempFile("images", "jpg")
+
+        imagesReference.getFile(localFile)
+                .addOnSuccessListener {
+                    successAction(localFile)
+                }
+                .addOnFailureListener(failureAction)
     }
 }
